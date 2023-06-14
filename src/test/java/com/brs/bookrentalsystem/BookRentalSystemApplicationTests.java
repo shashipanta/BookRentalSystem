cd55919dfaa0e61f369566cc7e5b5cfbe0196a62
pantashashi <shashi.panta@ausnepit.com.au>
2023-06-14 10:26:57 +0545

    Initial: Bootstraped Project Structure
    Created project structure with required dependency

---
+++ src/test/java/com/brs/bookrentalsystem/BookRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brs.bookrentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
