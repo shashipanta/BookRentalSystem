3533a421f963db9ba4c841c1fa721fc3b3035b8e
pantashashi <shashi.panta@ausnepit.com.au>
2023-06-17 16:07:53 +0545

    Update: Added template and fragments

--- src/main/java/com/brs/bookrentalsystem/controller/thymeleaf/HomeController.java
+++ src/main/java/com/brs/bookrentalsystem/controller/thymeleaf/HomeController.java
@@ -1,4 +1,19 @@
 package com.brs.bookrentalsystem.controller.thymeleaf;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/brs")
 public class HomeController {
+
+    //http://localhost:8080/brs/dashboard
+
+    @GetMapping(value = "/")
+    public String getDashboard(Model model){
+        return "index";
+    }
 }
