133e88f429ec7a3d0ef949f721a1456809fa535c
pantashashi <shashi.panta@ausnepit.com.au>
2023-06-19 18:11:41 +0545

    update: Created toast message for exception passed from controllerAdvice

--- src/main/java/com/brs/bookrentalsystem/error/ErrorResponse.java
+++ src/main/java/com/brs/bookrentalsystem/error/ErrorResponse.java
@@ -3,9 +3,11 @@ package com.brs.bookrentalsystem.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
     private String code;
     private String message;
--- src/main/java/com/brs/bookrentalsystem/error/handler/ControllerExceptionHandler.java
+++ src/main/java/com/brs/bookrentalsystem/error/handler/ControllerExceptionHandler.java
@@ -4,13 +4,18 @@ package com.brs.bookrentalsystem.error.handler;
 import com.brs.bookrentalsystem.error.ErrorResponse;
 import com.brs.bookrentalsystem.error.codes.ErrorCodes;
 import com.brs.bookrentalsystem.error.exception.NoSuchElementFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
+@Controller
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
@@ -29,5 +34,28 @@ public class ControllerExceptionHandler {
         mv.setViewName("exception-page");
         return mv;
     }
+
+    @ExceptionHandler(Exception.class)
+    public String handleDatabaseException(final Exception ex,
+                                          HttpServletRequest request,
+                                          RedirectAttributes ra){
+        ErrorResponse errorResponse = new ErrorResponse();
+
+
+        System.out.println("Context Path : " + request.getContextPath());
+
+        if(ex instanceof DataIntegrityViolationException){
+            errorResponse.setCode("S001");
+            errorResponse.setMessage("foreign key violation");
+
+            ra.addFlashAttribute("errorResponse", errorResponse);
+//            mv.setViewName("brs/admin/author/");
+//            mv.setViewName("author/author-page");
+        }
+        logger.error("foreign key violation exception occured");
+
+        return "redirect:/brs/admin/author/";
+    }
+
 
 }
